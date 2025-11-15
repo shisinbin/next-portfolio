6d37d5c81787de9f05750981b3539b8de51d81c4
ssb <sb1664@gmail.com>
2025-11-15 15:34:39 +0000

    refactor: replace JS height animation for ExpandableSection with CSS Grid accordion trick

--- src/components/ExpandableSection/ExpandableSection.js
+++ src/components/ExpandableSection/ExpandableSection.js
@@ -7,23 +7,15 @@ import styles from './ExpandableSection.module.css';
 
 function ExpandableSection({ children, defaultOpen = false }) {
   const [isOpen, setIsOpen] = React.useState(defaultOpen);
-  const [height, setHeight] = React.useState(0);
-  const contentRef = React.useRef(null);
-
-  const toggleOpen = () => setIsOpen((prev) => !prev);
-
-  React.useEffect(() => {
-    if (isOpen && contentRef.current) {
-      const scrollHeight = contentRef.current.scrollHeight;
-      setHeight(scrollHeight + 48);
-    } else {
-      setHeight(0);
-    }
-  }, [isOpen]);
 
   return (
-    <section className={styles.wrapper}>
-      <button className={styles.headerButton} onClick={toggleOpen}>
+    <section
+      className={`${styles.wrapper} ${isOpen ? styles.open : ''}`}
+    >
+      <button
+        className={styles.headerButton}
+        onClick={() => setIsOpen((o) => !o)}
+      >
         <ChevronRight
           className={`${styles.icon} ${
             isOpen ? styles.iconOpen : ''
@@ -31,16 +23,84 @@ function ExpandableSection({ children, defaultOpen = false }) {
         />
         <span>Extra info</span>
       </button>
-      <div
-        className={styles.contentWrapper}
-        style={{ maxHeight: isOpen ? `${height}px` : '0px' }}
-      >
-        <div ref={contentRef} className={styles.content}>
-          {children}
-        </div>
+      <div className={styles.contentWrapper}>
+        <div className={styles.content}>{children}</div>
       </div>
     </section>
   );
 }
 
 export default ExpandableSection;
+
+// 'use client';
+
+// import React from 'react';
+// import { ChevronRight } from 'react-feather';
+
+// import styles from './ExpandableSection.module.css';
+
+// function ExpandableSection({ children, defaultOpen = false }) {
+//   const [isOpen, setIsOpen] = React.useState(defaultOpen);
+//   const [height, setHeight] = React.useState(0);
+//   const contentRef = React.useRef(null);
+
+//   const calculateHeight = React.useCallback(() => {
+//     if (isOpen && contentRef.current) {
+//       const scrollHeight = contentRef.current.scrollHeight;
+//       setHeight(scrollHeight + 48);
+//     }
+//   }, [isOpen]);
+
+//   const toggleOpen = () => setIsOpen((prev) => !prev);
+
+//   // Recalculate height when opened
+//   React.useEffect(() => {
+//     calculateHeight();
+//   }, [isOpen, calculateHeight]);
+
+//   // Recalculate height on window resize
+//   React.useEffect(() => {
+//     if (!isOpen) return;
+
+//     let resizeFrame = null;
+
+//     const handleResize = () => {
+//       if (resizeFrame) {
+//         cancelAnimationFrame(resizeFrame);
+//       }
+//       resizeFrame = requestAnimationFrame(() => {
+//         calculateHeight();
+//       });
+//     };
+
+//     window.addEventListener('resize', handleResize);
+
+//     return () => {
+//       window.removeEventListener('resize', handleResize);
+//       if (resizeFrame) cancelAnimationFrame(resizeFrame);
+//     };
+//   });
+
+//   return (
+//     <section className={styles.wrapper}>
+//       <button className={styles.headerButton} onClick={toggleOpen}>
+//         <ChevronRight
+//           className={`${styles.icon} ${
+//             isOpen ? styles.iconOpen : ''
+//           }`}
+//         />
+//         <span>Extra info</span>
+//       </button>
+//       <div
+//         className={styles.contentWrapper}
+//         style={{ maxHeight: isOpen ? `${height}px` : '0px' }}
+//       >
+//         <div ref={contentRef} className={styles.content}>
+//           {children}
+//         </div>
+//       </div>
+//     </section>
+//   );
+// }
+
+// export default ExpandableSection;
